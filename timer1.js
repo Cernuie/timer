bdf5437104178bee833f81a2f860ab97f56e9915
Alexander Nguyen <cernvii@gmail.com>
2021-01-12 20:20:28 +0000

    added comments

--- timer1.js
+++ timer1.js
@@ -1,15 +1,15 @@
 'use strict';
-
+//read the arguments and sort them for looping
 let time = process.argv.slice(2);
 time = time.sort()
 
 const beeper = timer => {
-
+  //check if theres any strings so we can remove them from the array
   for (let sec of timer) {
     if (typeof(sec) === "string"){
       timer.splice(timer.indexOf(sec), 1);
     }
-  }
+  } //then loop through and set timers
   for (let sec of timer) {
     setTimeout(() => {
       process.stdout.write('\x07');
